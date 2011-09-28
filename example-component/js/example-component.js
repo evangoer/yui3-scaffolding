b159aaed2af42e9de6898ca32d54a1b1eb13b068
Evan Goer <evan@goer.org>
2011-09-27 21:30:43 -0700

    Updated the module to be a Y.Base-derived component.

--- example-component/js/example-component.js
+++ example-component/js/example-component.js
@@ -2,19 +2,28 @@
 YUI.add('example-component', function (Y) {  
     // Define all module code inside this anonymous function.
     
-    // A private member that will be closed over.
-    var greeting = "HELLOSKI";
-    
-    // Add public objects, methods and properties to the Y. 
-    Y.Example = {};
-    
-    Y.Example.hello = function () {
-        return greeting;
-    }   
+    // Y.Base is the building block for YUI components. Here we're using
+    // the Y.Base.create() sugar method rather than vanilla Y.extend().
+    Y.Example = Y.Base.create('example-component', Y.Base, [], {
+        
+        // Define your component's properties and methods here.
+        hello: function () {
+            return this.get('greeting');
+        }
+    }, {
+        // Define attributes on the special ATTRS object. 
+        ATTRS: {
+            // The 'greeting' attribute just has a default value set. 
+            // In general, attributes can have many fancy features. 
+            greeting: {
+                value: 'HI'
+            }
+        }
+    })
 }, 
 // An arbitrary string version number for your module.
 '0.1', 
 { 
     // An array of string module names that your module depends upon.
-    requires: []
+    requires: ['base']
 });
