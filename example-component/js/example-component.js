9c5f9a2f502ec15d84f0958d1ab04a322c4d6037
Evan Goer <evan@goer.org>
2011-09-27 21:08:49 -0700

    Added base files for example-component.

---
+++ example-component/js/example-component.js
@@ -0,0 +1,20 @@
+// Module names should be lower case and use dashes for a separator.
+YUI.add('example-basic', function (Y) {  
+    // Define all module code inside this anonymous function.
+    
+    // A private member that will be closed over.
+    var greeting = "HELLOSKI";
+    
+    // Add public objects, methods and properties to the Y. 
+    Y.Example = {};
+    
+    Y.Example.hello = function () {
+        return greeting;
+    }   
+}, 
+// An arbitrary string version number for your module.
+'0.1', 
+{ 
+    // An array of string module names that your module depends upon.
+    requires: []
+});
