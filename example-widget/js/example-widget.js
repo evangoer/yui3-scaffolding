b2a93953f9e36f4b2a159e12fcf051865755d862
Evan Goer <evan@goer.org>
2011-09-27 21:56:51 -0700

    Added empty skeleton widget. Does no work, will not pass tests.

--- example-widget/js/example-widget.js
+++ example-widget/js/example-widget.js
@@ -1,23 +1,47 @@
-// Module names should be lower case. If you need a separator, use dashes
-// as in 'my-example', not camel case, periods, or underscores.
-YUI.add('example', function (Y) {  
+// Module names should be lower case and use dashes for a separator.
+YUI.add('example-widget', function (Y) {  
     // Define all module code inside this anonymous function.
     
-    // A private member that will be closed over.
-    var greeting = "HELLOSKI";
-    
-    // Add public objects, methods and properties to the Y. 
-    Y.Example = {};
-    Y.Example.hello = function () {
-        return greeting;
-    }   
-}, 
+    // Y.Widget is the building block for YUI widgets. Here we're using
+    // the Y.Base.create() sugar method rather than vanilla Y.extend().
+    Y.Example = Y.Base.create('example-widget', Y.Widget, [], {
+        // Define your widget's properties and methods here.
+        
+        // Custom setup logic. The constructor automatically calls all
+        // initializer() methods in this object's inheritance chain.
+        initializer: function () {},
+
+        // Custom teardown logic. Calling destroy() calls all
+        // destructor() methods in this object's inheritance chain.
+        destructor: function () {},
+        
+        // Calling render() calls the following three methods in order: 
+        // 1. Set up the initial markup your widget needs.
+        renderUI: function () {},
+        
+        // 2. Bind event listeners to nodes on the page.
+        bindUI: function () {},
+        
+        // 3. Refresh the widget's appearance according to its current state.
+        //    You can call this method again in response to attribute changes.
+        syncUI: function() {}
+    }, {
+        // Define attributes on the special ATTRS object. 
+        ATTRS: {
+            // The 'greeting' attribute just has a default value set. 
+            // In general, attributes can have many fancy features. 
+            greeting: {
+                value: 'HI'
+            }
+        }
+    })
+},
 // An arbitrary string version number for your module.
 '0.1', 
 { 
     // An array of string module names that your module depends upon.
-    requires: [], 
+    requires: ['widget', 'base-build'], 
     
-    // Remove or set to false if your component does not have a skin.
+    // Remove or set to false if your module does not have a skin.
     skinnable: true
 });
